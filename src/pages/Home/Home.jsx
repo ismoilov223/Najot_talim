0a9555fed25d4bdfb5bdeb2b640e3cdcb5e97417
ismoilov223 <shukurulloismolilov832@gmail.com>
2023-08-19 11:11:25 +0500

    togrladm

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,21 +2,13 @@ import React from "react";
 import { HomeStyled } from "./Home.Styled";
 
 const Home = () => {
-
   return (
     <HomeStyled className="home">
-      <div className="home__header"></div>
+      <div className="home__header">
+        <h1>Home</h1>
+      </div>
     </HomeStyled>
   );
-
-	return (
-		<HomeStyled className='home'>
-			<div className='home__header'>
-				<h1>Home</h1>
-			</div>
-		</HomeStyled>
-	);
-
 };
 
 export default Home;
