3a08c5f45f745caac464215f068c8b3f1a84542e
Alicoder001 <coderkuchkarov@gmail.com>
2023-08-19 13:03:02 +0500

    navbar container

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
-import "./App.css";
-import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import { useState } from 'react';
+import './App.css';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
 function App() {
-  return (
-    <div className="App">
-      <Navbar></Navbar>
-      <Outlet />
-      <Footer></Footer>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Navbar />
+			<Outlet />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
